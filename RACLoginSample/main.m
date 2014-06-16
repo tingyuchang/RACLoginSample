e4b647ad26fd1656544048b572d8d9824c4ddba9
Matt Chang <mattchang@accuvally.com>
2014-06-16 19:22:27 +0800

    Initial Commit

---
+++ RACLoginSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RACLoginSample
+//
+//  Created by Matt Chang on 2014/6/16.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
---
+++ RACLoginSampleTests/RACLoginSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  RACLoginSampleTests.m
+//  RACLoginSampleTests
+//
+//  Created by Matt Chang on 2014/6/16.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RACLoginSampleTests : XCTestCase
+
+@end
+
+@implementation RACLoginSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
