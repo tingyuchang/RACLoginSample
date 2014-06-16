57bcd795129ef72d56d56ef9a14eade59736a684
Matt Chang <mattchang@accuvally.com>
2014-06-16 22:31:16 +0800

    add viewController & viewModel

---
+++ RACLoginSample/TTLoginViewModel.h
@@ -0,0 +1,13 @@
+//
+//  TTLoginViewModel.h
+//  RACLoginSample
+//
+//  Created by Matt Chang on 2014/6/16.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTLoginViewModel : NSObject
+
+@end
---
+++ RACLoginSample/TTLoginViewModel.m
@@ -0,0 +1,13 @@
+//
+//  TTLoginViewModel.m
+//  RACLoginSample
+//
+//  Created by Matt Chang on 2014/6/16.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import "TTLoginViewModel.h"
+
+@implementation TTLoginViewModel
+
+@end
